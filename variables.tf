bd159fae3f9ab0b29c79ba9faa793f5f5e3eaf48
dibisis <dibisis@gmail.com>
2020-06-15 04:04:29 +0900

    typo correction

--- variables.tf
+++ variables.tf
@@ -10,6 +10,7 @@ DESCRIPTION
 
 variable "key_name" {
   description = "Desired name of AWS key pair"
+
 }
 
 variable "aws_region" {
@@ -20,7 +21,7 @@ variable "aws_region" {
 # ubuntu 16.04
 variable "aws_amis" {
   default = {
-    ap-northeast-2 = ""ami-0761dd1f89936259e""
+    ap-northeast-2 = "ami-0761dd1f89936259e"
 //    us-east-1 = "ami-1d4e7a66"
 //    us-west-1 = "ami-969ab1f6"
 //    us-west-2 = "ami-8803e0f0"
